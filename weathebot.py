b1324da1b6e1eb7ff15f0381e4f5e78caa712f18
max <max.iw15@yandex.ru>
2022-09-03 07:05:17 +0300

    Убран тестовый режим

--- weathebot.py
+++ weathebot.py
@@ -10,7 +10,7 @@ while True:
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     time.sleep(60)
-    if current_time[3:5] == '00':
+    if current_time == '6:00' or "17:30":
         answer = (weather_want_know())
         bot.send_message(467322175, f"Доброе утро.\nНа улице {answer[0]} \n"
                                     f"Температура в настоящий момент {round(int(answer[1]))} градусов.\n"
