a67c64349fe3cc7aa22b99bbf89ec32e37a49a39
max <max.iw15@yandex.ru>
2022-09-03 14:31:41 +0300

    Добавлены первые наработки для яндекс транспорта

--- yandex.py
+++ yandex.py
@@ -41,12 +41,12 @@ def weather_want_know():
 
 def transport_want_know():
     try:
-        res = requests.get("https://api.rasp.yandex.net/v3.0/stations_list/?",
-                           params={'apikey': '548ab260-4f49-4ba5-9779-c05eeacfb9a0', 'lang': 'ru_RU', 'format': 'json',
-                                   'hours': "true"})
+        res = requests.get("https://api.rasp.yandex.net/v3.0/search/?",
+                           params={'from': "s9600731", "to": "s2001005", 'apikey': '548ab260-4f49-4ba5-9779-c05eeacfb9a0', 'format': 'json'})
 
         data = res.json()
     finally:
-        print(data)
+        return data
 
 
+answer = transport_want_know()
