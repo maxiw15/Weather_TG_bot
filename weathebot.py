7cb1f0c35617636f2be43644412b7b6dc7769483
max <max.iw15@yandex.ru>
2022-09-06 17:28:56 +0300

    Добавлена возможность узнать погоду за различные временные промежутки в течении дня

--- weathebot.py
+++ weathebot.py
@@ -10,8 +10,11 @@ while True:
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     time.sleep(60)
-    if current_time == '06:00' or current_time == "17:40":
+    if current_time == '06:00':
         answer = weather_want_know()
         bot.send_message(467322175, f"Доброе утро.\n {answer}")
+    if current_time == "17:40":
+        answer = weather_want_know()
+        bot.send_message(467322175, f"Добрый вечер.\n {answer}")
 
 
--- yandex.py
+++ yandex.py
@@ -30,23 +30,32 @@ def weather_want_know():
                                         'X-Yandex-API-Key': 'c8ff1703-12da-4776-89ca-55858ebd74da'}, headers=headers)
         data = res.json()
     finally:
-        return f'Температура {data["fact"]["temp"]} градусов. \n' \
+        return f'В настоящий момент Температура {data["fact"]["temp"]} градусов. \n' \
                f'Ощущается как {data["fact"]["feels_like"]} градусов. \n' \
                f'{weather_dict[data["fact"]["condition"]]}. \n' \
                f'Скорость ветра {data["fact"]["wind_speed"]} м/с. \n' \
                f'{thunderstrom_maybe[data["fact"]["is_thunder"]]}. \n' \
                f'Вероятность осадков {data["fact"]["prec_type"]}. \n' \
-               f'Средняя температура вечером {data["forecasts"][0]["parts"]["evening"]["temp_avg"]} градусов.'
+               f'Средняя температура вечером {data["forecasts"][0]["parts"]["evening"]["temp_avg"]} градусов. \n' \
+               f'Температура в 10 утра {data["forecasts"][0]["hours"][10]["temp"]} градусов. \n' \
+               f'Будет {weather_dict[data["forecasts"][0]["hours"][10]["condition"]]}. \n' \
+               f'Температура в 14 часов {data["forecasts"][0]["hours"][14]["temp"]} градусов. \n' \
+               f'Будет {weather_dict[data["forecasts"][0]["hours"][14]["condition"]]}. \n' \
+               f'Температура в 19 часов {data["forecasts"][0]["hours"][19]["temp"]}. \n' \
+               f'Будет {weather_dict[data["forecasts"][0]["hours"][19]["condition"]]} градусов. \n' \
+
 
 
 def transport_want_know():
     try:
         res = requests.get("https://api.rasp.yandex.net/v3.0/search/?",
-                           params={'from': "s9600731", "to": "s2001005", 'apikey': '548ab260-4f49-4ba5-9779-c05eeacfb9a0', 'format': 'json'})
+                           params={'from': "s9600731", "to": "s2001005", 'apikey': '548ab260-4f49-4ba5-9779'
+                                                                                   '-c05eeacfb9a0', 'format': 'json'})
 
         data = res.json()
     finally:
         return data
 
 
-answer = transport_want_know()
+answer = weather_want_know()
+print(answer)
