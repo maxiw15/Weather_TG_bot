2d1416eb93e3cd2067b2395a357e96ab17f1614c
max <max.iw15@yandex.ru>
2022-10-09 11:57:57 +0300

    Баг фикс

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot
 from yandex import weather_want_know
 
-
 token = "5572813010:AAF18_DlYPryC6-GXcIswRG2q5QTZfWlavA"
 bot = telebot.TeleBot(token)
 
@@ -10,6 +9,7 @@ bot = telebot.TeleBot(token)
 def send_welcome(message):
     answer = weather_want_know()
     bot.reply_to(message, answer)
+    bot.reply_to(message, "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg")
 
 
 @bot.message_handler(func=lambda message: True)
--- weathebot.py
+++ weathebot.py
@@ -8,22 +8,21 @@ bot = telebot.TeleBot(token)
 
 
 def time_weather(morning, evening, id):
-    while True:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M")
-        time.sleep(60)
-        if current_time == '06:00' == morning:
-            answer = weather_want_know()
-            print(answer)
-            bot.send_photo(id, "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg")
-            bot.send_message(id, f"Доброе утро.\n{answer}")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    time.sleep(60)
+    if current_time == morning:
+        answer = weather_want_know()
+        bot.send_photo(id, "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg")
+        bot.send_message(id, f"Доброе утро.\n{answer}")
 
-        if current_time == "17:40" == evening:
-            answer = weather_want_know()
-            bot.send_photo(id, "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg")
-            bot.send_message(id, f"Добрый вечер.\n{answer}")
+    if current_time == evening:
+        answer = weather_want_know()
+        bot.send_photo(id, "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg")
+        bot.send_message(id, f"Добрый вечер.\n{answer}")
 
 
-time_weather("09:00", "17:40", 467322175)
-time_weather("09:00", "17:40", 551376473)
-time_weather("09:00", "17:40", 488717704)
+while True:
+    time_weather("09:00", "17:40", 467322175)
+    time_weather("09:00", "17:40", 551376473)
+    time_weather("09:00", "17:40", 488717704)
